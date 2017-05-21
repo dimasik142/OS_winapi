0a89931b1b88a06d66604da03610baa6f2bb1b7d
dimasik142 <dimasik142>
2017-05-21 04:45:09 +0300

    Add 4 laba

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<windows.h>
 #include<string.h>
 #include <ctime>
+#include <fstream>
+
 
 #define BLOCK_HEIGHT 100
 
@@ -8,14 +10,26 @@ using namespace std;
 
 BOOL Line(HDC,int,int,int,int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void returnToMapRandAuto();
+HANDLE handeForChangeColor, handeForAddNewAuto, handeForCheckThreeAuto;
 
-int positionAutos[3] = {-100,-1000,-1000};
-const int yCordinates[3][2] = {{30,170},{230, 370},{430, 570} }; // масив кординат по осі y
+CRITICAL_SECTION critic_section;
+
+DWORD WINAPI addAutoToMap(LPVOID);
+DWORD WINAPI changeAutosAndMapColor(LPVOID);
+DWORD WINAPI checkToTheThreeAutos(LPVOID);
+
+int autosColor[3] = {111,111,111};
+int positionAutos[5][2] = { {1000,3},{1000,3},{1000,3},{1000,3},{1000,3} };
+const int yCordinates[3][2] = {{30,170},{230, 370},{430, 570} };
+
+const int priorityProcessArray[3] = { 
+	2 ,  // addAutoToMap
+	1 ,  // changeAutosColor
+	3    // chackAutos
+};
+
 char szProgName[] = "Егра";
-int timeStatus = 0, playerPosition = 1 , saveAuto = 4;
-
-
+int timeStatus = 0, playerPosition = 1 , saveAuto = 100, saveAutoTime = 0 , AS_mode = 0;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -23,22 +37,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	MSG lpMsg;
 	WNDCLASS w;
 
-	w.lpszClassName = L"Егра"; //имя программы - объявлено выше
-	w.hInstance = hInstance; //идентификатор текущего приложения
-	w.lpfnWndProc = WndProc; //указатель на функцию окна
-	w.hCursor = LoadCursor(NULL, IDC_ARROW); //загружаем курсор
+	w.lpszClassName = L"Егра"; 
+	w.hInstance = hInstance;
+	w.lpfnWndProc = WndProc;
+	w.hCursor = LoadCursor(NULL, IDC_ARROW); 
 	w.hIcon = 0;
 	w.lpszMenuName = 0;
-	w.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //цвет фона окна
+	w.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	w.style = CS_HREDRAW | CS_VREDRAW;
 	w.cbClsExtra = 0;
 	w.cbWndExtra = 0;
 
-	//Если не удалось зарегистрировать класс окна - выходим
 	if (!RegisterClass(&w))
 		return 0;
 
-	//Создадим окно в памяти, заполнив аргументы CreateWindow
 	hWnd = CreateWindow(L"Егра", L"Тіпа егра", WS_OVERLAPPED | WS_BORDER | WS_SYSMENU, 
 		300,0,600,700, 
 		(HWND)NULL, 
@@ -48,12 +60,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	  
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
-
 	while (GetMessage(&lpMsg, NULL, 0, 0)) {
 		TranslateMessage(&lpMsg);
 		DispatchMessage(&lpMsg);
 	}
-
+	if (AS_mode == 0) {
+		DeleteCriticalSection(&critic_section);
+	}
 	return(lpMsg.wParam);
 }
 
@@ -62,56 +75,101 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam) {
 	
 	HDC hdc; 
 	PAINTSTRUCT ps; 
-	RECT auto1 , auto2, auto3 , player;
+	RECT autos[5], player;
+	HANDLE thread;
+	HBRUSH Brush;
+
+	int delta;
+
 
 	switch (messg) {
+
+
 		case WM_CREATE:
+			if (AS_mode == 0) {
+				InitializeCriticalSection(&critic_section);
+			}
 			SetTimer(hWnd, 1, 50, NULL);
+			handeForAddNewAuto = CreateThread(NULL, 0, addAutoToMap, NULL, 0, NULL);
 			SetForegroundWindow(hWnd);
-			returnToMapRandAuto();
-			returnToMapRandAuto();
 			break;
+
+
 		case WM_TIMER:
+
 			srand(timeStatus);
 			timeStatus++;
-			if (positionAutos[playerPosition] + BLOCK_HEIGHT > 550 && positionAutos[playerPosition] < 650) {
-				KillTimer(hWnd, 1);
-				MessageBox(NULL, (LPCTSTR)L"канец", L"канец", MB_ICONASTERISK | MB_OK);
-				PostQuitMessage(0);
+			
+			for (int item = 0; item < 5; item++) {
+				if (positionAutos[item][0] + BLOCK_HEIGHT > 550 && positionAutos[item][0] < 650 && positionAutos[item][1] == playerPosition) {
+					KillTimer(hWnd, 1);
+					MessageBox(NULL, (LPCTSTR)L"канец", L"канец", MB_ICONASTERISK | MB_OK);
+					PostQuitMessage(0);
 
+				}
+				positionAutos[item][0] += 8 + (int)(timeStatus / 50);
 			}
-			for (int i = 0; i < 3; i++) {
-				positionAutos[i] += 8 + (int)(timeStatus / 50);
+
+			delta = 30 - (int)(timeStatus / 50);
+			if (delta < 10)
+				delta = 10;
+
+			if ((timeStatus % delta) == 0) {
+				if (saveAutoTime != 0) {
+					saveAutoTime--;
+				}
+
+				handeForAddNewAuto = CreateThread(NULL, 0, addAutoToMap, NULL, 0, NULL);
+				SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+				SetThreadPriority(handeForAddNewAuto, priorityProcessArray[0]);
+				//CloseHandle(handeForAddNewAuto);
+
+				handeForCheckThreeAuto = CreateThread(NULL, 0, checkToTheThreeAutos, NULL, 0, NULL);
+				SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+				SetThreadPriority(handeForCheckThreeAuto, priorityProcessArray[2]);
+				//CloseHandle(handeForCheckThreeAuto);
 			}
-			if ((timeStatus %40 - +(int)(timeStatus / 50)) == 0) {
-				returnToMapRandAuto();
-				returnToMapRandAuto();
-			}
-			if (positionAutos[1] < 400 && positionAutos[0] < 400 && positionAutos[2] < 400) {
-				positionAutos[rand() % 3] = 10000;
-			}
+
 			InvalidateRect(hWnd, NULL, TRUE);
 			break;
+
+
 		case WM_PAINT:
 			hdc = BeginPaint(hWnd, &ps);
+			for (int i = 0; i < 5; i++) {
+				switch (positionAutos[i][1])
+				{
+					case 0:
+						autos[i].left = yCordinates[0][1];
+						autos[i].right = yCordinates[0][0];
+						break;
+					case 1:
+						autos[i].left = yCordinates[1][1];
+						autos[i].right = yCordinates[1][0];
+						break;
+					case 2:
+						autos[i].left = yCordinates[2][1];
+						autos[i].right = yCordinates[2][0];
+						break;
+					default :
+						break;
+				}
 
-			auto1.top = positionAutos[0] + BLOCK_HEIGHT;
-			auto1.left = yCordinates[0][1];
-			auto1.right = yCordinates[0][0];
-			auto1.bottom = positionAutos[0];
-			FillRect(hdc, &auto1, HBRUSH(CreateSolidBrush(RGB(111, 111, 111))));
-
-			auto2.top = positionAutos[1] + BLOCK_HEIGHT;
-			auto2.left = yCordinates[1][1];
-			auto2.right = yCordinates[1][0];
-			auto2.bottom = positionAutos[1];
-			FillRect(hdc, &auto2, HBRUSH(CreateSolidBrush(RGB(111, 111, 111))));
-		
-			auto3.top = positionAutos[2] + BLOCK_HEIGHT;
-			auto3.left = yCordinates[2][1];
-			auto3.right = yCordinates[2][0];
-			auto3.bottom = positionAutos[2];
-			FillRect(hdc, &auto3, HBRUSH(CreateSolidBrush(RGB(111, 111, 111))));
+				autos[i].top = positionAutos[i][0] + BLOCK_HEIGHT;
+				autos[i].bottom = positionAutos[i][0];
+				if (saveAuto != 100) {
+					if (saveAuto == positionAutos[i][1] && saveAutoTime > 0) {
+						Brush = CreateSolidBrush(RGB(0, 255, 0));
+						FillRect(hdc, &autos[i], Brush);
+						DeleteObject(Brush);
+					}
+				}
+				else {
+					Brush = CreateSolidBrush(RGB(autosColor[0], autosColor[1], autosColor[2]));
+					FillRect(hdc, &autos[i], Brush);
+					DeleteObject(Brush);
+				}
+			}
 
 			player.top = 650;
 			player.left = yCordinates[playerPosition][1];
@@ -124,7 +182,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam) {
 		
 			EndPaint(hWnd, &ps);
 			break;
+
+
 		case WM_KEYDOWN:
+			handeForChangeColor = CreateThread(NULL, 0, changeAutosAndMapColor, NULL, 0, NULL);
+			SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+			SetThreadPriority(handeForAddNewAuto, priorityProcessArray[1]);
+			//CloseHandle(handeForChangeColor);
+
 			switch (wParam) {
 				case VK_LEFT:
 					if (playerPosition != 0)
@@ -140,9 +205,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam) {
 					break;
 			}
 			break;
+
+
 		case WM_DESTROY:
 			PostQuitMessage(0); 
 			break;
+
 
 		default:
 			return(DefWindowProc(hWnd, messg, wParam, lParam)); 
@@ -156,9 +224,73 @@ BOOL Line(HDC hdc, int x1, int y1, int x2, int y2)
 	return LineTo(hdc, x2, y2);
 }
 
-void returnToMapRandAuto() {
-	int autoNumber = rand() % 3;
-	if (positionAutos[autoNumber] > 700 ) {
-		positionAutos[autoNumber] = -100;
+DWORD WINAPI addAutoToMap(LPVOID t1)
+{
+	if (AS_mode == 0)
+	{
+		while (!TryEnterCriticalSection(&critic_section));
 	}
+	srand(time(0));
+	int autoNumber = rand() % 5;
+	bool addAutoStatus = false;
+
+	while (!addAutoStatus) {
+		if (positionAutos[autoNumber][0] > 700 || positionAutos[autoNumber][0] < -150) {
+			positionAutos[autoNumber][1] = rand() % 3;
+			positionAutos[autoNumber][0] = -100;
+			addAutoStatus = true;
+		}
+		srand((int)t1);
+		int autoNumber = rand() % 5;
+	}
+	if (AS_mode == 0) {
+		LeaveCriticalSection(&critic_section);
+	}
+
+	return 0;
+}
+
+DWORD WINAPI changeAutosAndMapColor(LPVOID t)
+{
+	if (AS_mode == 0)
+	{
+		while (!TryEnterCriticalSection(&critic_section));
+	}
+	srand(time(0));
+
+	for (int i = 0; i < 3; i++) {
+		autosColor[i] = rand() % 255;
+	}
+	if (AS_mode == 0) {
+		LeaveCriticalSection(&critic_section);
+	}
+	return 0;
+}
+
+DWORD WINAPI checkToTheThreeAutos(LPVOID t)
+{
+	int checkAutosPosition[5][2];
+	int checkPoint = 0;
+
+	for (int i = 0; i < 5; i++) {
+		if ( positionAutos[i][0] < 600  && positionAutos[i][0] > 300 ) {
+			checkAutosPosition[checkPoint][0] = positionAutos[i][0];
+			checkAutosPosition[checkPoint][1] = positionAutos[i][1];
+			checkPoint++;
+		}
+	}
+
+	bool checkLinesByAutos[3] = { FALSE , FALSE , FALSE };
+
+	for (int i = 0; i < checkPoint; i++) {
+		checkLinesByAutos[checkAutosPosition[i][1]] = TRUE;
+	}
+
+	if (checkLinesByAutos[0] == FALSE || checkLinesByAutos[1] == FALSE || checkLinesByAutos[2] == FALSE)
+		return 0;
+
+	srand(time(0));
+	saveAuto = rand() % 3;
+	saveAutoTime = 5;
+	return 0;
 }
